14f8b6fc95c2be7cae14ebc835d005e753390029
sadness_ojisan <sadness.ojisan@gmail.com>
2021-04-27 05:05:17 +0900

    del

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ console.log("hello world!!");
 function handleFormSubmit() {
   const input = document.getElementById("task-input");
   const inputValue = input.value; // 厳密には instanceof で input要素であるかのチェックを入れるべき
+  if (!inputValue.length > 0) {
+    alert("テキストを入力してください。");
+    return;
+  }
   const todosEl = document.getElementById("todos");
   const todoEl = createTodoElement(inputValue);
   todosEl.appendChild(todoEl);
@@ -21,10 +25,26 @@ function handleFormSubmit() {
 function createTodoElement(inputValue) {
   const todoEl = document.createElement("li");
   const checkBoxEl = document.createElement("input");
+  todoEl.appendChild(checkBoxEl);
   const labelEl = document.createElement("label");
   labelEl.innerText = inputValue;
   checkBoxEl.type = "checkbox";
-  todoEl.appendChild(checkBoxEl);
+  checkBoxEl.onchange = function (e) {
+    const checked = e.target.checked;
+    if (checked) {
+      todoEl.className = `checked`;
+    } else {
+      todoEl.className = "";
+    }
+  };
   todoEl.appendChild(labelEl);
+
+  const buttonEl = document.createElement("button");
+  buttonEl.innerText = "削除";
+  buttonEl.onclick = function () {
+    todoEl.remove();
+  };
+  todoEl.appendChild(buttonEl);
+
   return todoEl;
 }
