4d1b9b20dec2d33ef95762a41f36998bf44112db
sadness_ojisan <sadness.ojisan@gmail.com>
2021-04-27 04:42:18 +0900

    add todo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log("hello world!!");
+
+/**
+ * formの送信時に実行する関数。
+ * input form の入力内容を取得してそれをTODOリストに加える
+ */
+function handleFormSubmit() {
+  const input = document.getElementById("task-input");
+  const inputValue = input.value; // 厳密には instanceof で input要素であるかのチェックを入れるべき
+  const todosEl = document.getElementById("todos");
+  const todoEl = document.createElement("li");
+  todoEl.innerText = inputValue;
+  todosEl.appendChild(todoEl);
+  input.value = ""; // input のリセット
+}
