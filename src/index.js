e785a71b0cf1c8e6e65fa20a7f66646db3bdc6ab
sadness_ojisan <sadness.ojisan@gmail.com>
2021-04-27 04:48:07 +0900

    add checkbox el cretor

--- src/index.js
+++ src/index.js
@@ -8,8 +8,23 @@ function handleFormSubmit() {
   const input = document.getElementById("task-input");
   const inputValue = input.value; // 厳密には instanceof で input要素であるかのチェックを入れるべき
   const todosEl = document.getElementById("todos");
-  const todoEl = document.createElement("li");
-  todoEl.innerText = inputValue;
+  const todoEl = createTodoElement(inputValue);
   todosEl.appendChild(todoEl);
   input.value = ""; // input のリセット
+}
+
+/**
+ * TODO要素を作る関数
+ * @param {*} inputValue TODO文字列
+ * @returns TODO要素
+ */
+function createTodoElement(inputValue) {
+  const todoEl = document.createElement("li");
+  const checkBoxEl = document.createElement("input");
+  const labelEl = document.createElement("label");
+  labelEl.innerText = inputValue;
+  checkBoxEl.type = "checkbox";
+  todoEl.appendChild(checkBoxEl);
+  todoEl.appendChild(labelEl);
+  return todoEl;
 }
